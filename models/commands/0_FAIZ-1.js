5efabc83022ba55596ab92f8235379ca69c6e98d
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-31 07:49:52 +0530

    Update 0_FAIZ-1.js

--- models/commands/0_FAIZ-1.js
+++ models/commands/0_FAIZ-1.js
@@ -1,13 +1,10 @@
-//@Prem-babu3608
-////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
 module.exports.config = {
 	name: "IMOGE 1",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", 
-	description: "THIS BOT IS MR PREM SHARMA",
+	credits: "FAIZU", 
+	description: "THIS BOT IS MR FAIZA",
 	commandCategory: "NO PREFIX",
     cooldowns: 5, 
 };
