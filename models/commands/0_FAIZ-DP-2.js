d95d292fbc3c2fb356adbe4e6a478ca04ab9d2bd
Ariffaiz90 <payalthakur969491@gmail.com>
2024-11-03 07:56:15 +0530

    Update 0_FAIZ-DP-2.js

--- models/commands/0_FAIZ-DP-2.js
+++ models/commands/0_FAIZ-DP-2.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
  
   var background = [
  
-    "https://i.imgur.com/6UpYjGf.jpeg"
+    "https://i.imgur.com/YLSBTpi.jpeg"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
  
