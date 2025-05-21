759eca870bae3ad1884f4913ad9ad5108fd1ea32
ARAman7860 <khanbab7865431@gmail.com>
2025-05-21 13:05:47 +0530

    Update 0_🍎-FAIZ-BF.js

--- models/commands/0_🍎-FAIZ-BF.js
+++ models/commands/0_🍎-FAIZ-BF.js
@@ -20,7 +20,7 @@ module.exports.onLoad = async() => {
   const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { downloadFile } = global.utils;
   const dirMaterial = __dirname + `/cache/canvas/`;
-  const path = resolve(__dirname, 'cache/canvas', 'ara.png');
+  const path = resolve(__dirname, 'cache/canvas', 'ara.jpeg');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
   if (!existsSync(path)) await downloadFile("https://i.imgur.com/7LhQ9d8.jpeg", path); 
 }
@@ -32,7 +32,7 @@ async function makeImage({ one, two }) {
   const jimp = global.nodemodule["jimp"];
   const __root = path.resolve(__dirname, "cache", "canvas");
 
-  let batgiam_img = await jimp.read(__root + "/ara.png");
+  let batgiam_img = await jimp.read(__root + "/ara.jpeg");
   let pathImg = __root + `/batman${one}_${two}.png`;
   let avatarOne = __root + `/avt_${one}.png`;
   let avatarTwo = __root + `/avt_${two}.png`;
