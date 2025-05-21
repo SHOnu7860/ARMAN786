5917f129497539f7625130d8ffb21985bc706fc0
ARAman7860 <khanbab7865431@gmail.com>
2025-05-21 12:52:08 +0530

    Update 0_FAIZ-PAIR-MENTION-2.js

--- models/commands/0_FAIZ-PAIR-MENTION-2.js
+++ models/commands/0_FAIZ-PAIR-MENTION-2.js
@@ -68,7 +68,7 @@ module.exports.run = async function ({ event, api, args }) {
     const fs = global.nodemodule["fs-extra"];
     const { threadID, messageID, senderID } = event;
     const mention = Object.keys(event.mentions);
-    if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
+    if (!mention[0]) return api.sendMessage("kisi ek ko mention kar tutiya😹.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
         return makeImage({ one, two }).then(path => api.sendMessage({ body: "❥︎|======『  𝗟❤︎𝗩𝗲  』======|☻︎", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
