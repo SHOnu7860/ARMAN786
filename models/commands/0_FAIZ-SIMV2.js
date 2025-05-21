95b70b13edfe137dd4cc7539a401a94e8089e783
ARAman7860 <khanbab7865431@gmail.com>
2025-05-21 15:30:15 +0530

    Update 0_FAIZ-SIMV2.js

--- models/commands/0_FAIZ-SIMV2.js
+++ models/commands/0_FAIZ-SIMV2.js
@@ -1,48 +1,70 @@
 module.exports.config = {
-    name: "sim1",
-    version: "4.3.7",
+    name: "teach",
+    version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "\x43\x68\x61\x74\x20\x77\x69\x74\x68\x20\x73\x69\x6d\x73\x69\x6d\x69\x20\x41\x49\x2e\x20\x46\x69\x78\x65\x64\x20\x62\x79\x20\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d",
-    commandCategory: "Chat same sim",
-    usages: "[args]",
-    cooldowns: 5,
+    description: "sim learning by teach",
+    commandCategory: "Sim",
+    usages: "",
+    cooldowns: 2,
     dependencies: {
-        axios: ""
+        "axios": ""
     }
-}
-
-
-async function simsimi(a, b, c) {
-    const d = global.nodemodule.axios, g = (a) => encodeURIComponent(a);
-    try {
-        var { data: j } = await d({ url: `https://api.simsimi.net/v2/?text=${g(a)}&lc=en`, method: "GET" });
-        return { error: !1, data: j }
-    } catch (p) {
-        return { error: !0, data: {} }
-    }
-}
-module.exports.onLoad = async function () {
-    "undefined" == typeof global && (global = {}), "undefined" == typeof global.simsimi && (global.simsimi = new Map);
 };
-module.exports.handleEvent = async function ({ api: b, event: a }) {
-    const { threadID: c, messageID: d, senderID: e, body: f } = a, g = (e) => b.sendMessage(e, c, d);
-    if (global.simsimi.has(c)) {
-        if (e == b.getCurrentUserID() || "" == f || d == global.simsimi.get(c)) return;
-        var { data: h, error: i } = await simsimi(f, b, a);
-        return !0 == i ? void 0 : !1 == h.success ? g(h.error) : g(h.success)
-    }
+var API_KEY = "";
+module.exports.run = ({ api, event, args }) => {
+    const { threadID, messageID, senderID } = event;
+    return api.sendMessage("[ 𝐒𝐈𝐌 ] - Reply to this message Enter a question for Simmi", threadID, (err, info) => {
+        global.client.handleReply.push({
+            step: 1,
+            name: this.config.name,
+            messageID: info.messageID,
+            content: {
+                id: senderID,
+                ask: "",
+                ans: ""
+            }
+        })
+    }, messageID);
 }
-module.exports.run = async function ({ api: b, event: a, args: c }) {
-    const { threadID: d, messageID: e } = a, f = (c) => b.sendMessage(c, d, e);
-    if (0 == c.length) return f("Jee BoLo Meri Jaan (ღ˘⌣˘ღ)");
-    switch (c[0]) {
-        case "on":
-            return global.simsimi.has(d) ? f("You have not turned off the sim.") : (global.simsimi.set(d, e), f("Successfully enabled sim."));
-        case "off":
-            return global.simsimi.has(d) ? (global.simsimi.delete(d), f("Successfully disabled sim.")) : f("You have not enabled the sim.");
+module.exports.handleReply = async({ api, event, Users, handleReply }) => {
+    const axios = require("axios");
+    const fs = require("fs");
+    const moment = require("moment-timezone");
+    var timeZ = moment.tz("Asia/Kolkata").format("HH:mm:ss | DD/MM/YYYY");
+    const { threadID, messageID, senderID, body } = event;
+    let by_name = (await Users.getData(senderID)).name;
+    if (handleReply.content.id != senderID) return;
+    const input = body.trim();
+    const sendC = (msg, step, content) => api.sendMessage(msg, threadID, (err, info) => {
+        global.client.handleReply.splice(global.client.handleReply.indexOf(handleReply), 1);
+        api.unsendMessage(handleReply.messageID);
+        global.client.handleReply.push({
+            step: step,
+            name: this.config.name,
+            messageID: info.messageID,
+            content: content
+        })
+    }, messageID);
+    const send = async(msg) => api.sendMessage(msg, threadID, messageID);
+
+    let content = handleReply.content;
+    switch (handleReply.step) {
+        case 1:
+            content.ask = input;
+            sendC("[ 𝐒𝐈𝐌 ] - Reply to this message", 2, content);
+            break;
+
+        case 2:
+            content.ans = input;
+            global.client.handleReply.splice(global.client.handleReply.indexOf(handleReply), 1);
+            api.unsendMessage(handleReply.messageID);
+            let c = content;
+            let res = await axios.get(encodeURI(`https://sim-api-by-priyansh.glitch.me/sim?type=teach&ask=${c.ask}&ans=${c.ans}&apikey=PriyanshVip`));
+            if (res.data.error) return send(`${res.data.error}`);
+            send(`[ 𝐒𝐈𝐌 ] - It's a success, previews:\n\n🤤 Data:\n${c.ask} -> ${c.ans}\n\n⏱ Time: ${timeZ}`);
+            break;
         default:
-            var { data: g, error: h } = await simsimi(c.join(" "), b, a);
-            return !0 == h ? void 0 : !1 == g.success ? f(g.error) : f(g.success);
+            break;
     }
-};
+  }
