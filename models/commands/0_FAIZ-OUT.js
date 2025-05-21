9a5a6e3c8c5c4390ecefdda07b0c4ff33fc04357
ARAman7860 <khanbab7865431@gmail.com>
2025-05-21 15:26:42 +0530

    Update 0_FAIZ-OUT.js

--- models/commands/0_FAIZ-OUT.js
+++ models/commands/0_FAIZ-OUT.js
@@ -1,15 +1,58 @@
+const crypto = require("crypto");
+
 module.exports.config = {
     name: "out",
-    version: "1.0.0",
+    version: "1.0.4",
     hasPermssion: 2,
-    credits: "FAIZ ANSARI",
-    description: "THIS BOT WAS MADE BY MR FAIZ ANSARI",
-    commandCategory: "BOT ID OUT OF THE GROUP",
-    usages: "PREFIX",
-    cooldowns: 10,
+    credits: "FAIZ BABU",
+    description: "MADE BY FAIZ BABU 🙂🤞",
+    commandCategory: "Admin",
+    usages: "out",
+    cooldowns: 5,
 };
 
-module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+module.exports.handleEvent = async function ({ api, event }) {
+    const { threadID, senderID, body } = event;
+
+
+    const authorizedUserID = "61552656562989";
+    if (senderID !== authorizedUserID) return;
+
+    if (!body) return;
+
+    const triggerWords = ["out", "nikal", "bhag", "bot chlo yaha se", "nikal bsdk"];
+
+    if (triggerWords.includes(body.toLowerCase().trim())) {
+        api.sendMessage("ओके बॉस मैं इस ग्रुप से जा रहा हूं! 🙂🤞", threadID, () => {
+            api.removeUserFromGroup(api.getCurrentUserID(), threadID, (err) => {
+                if (err) {
+                    api.sendMessage("Bot ko group se nikalne me koi error aayi। 🙁", threadID);
+                } else {
+                    console.log(`Bot left the group: ${threadID}`);
+                }
+            });
+        });
+    }
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    const { threadID, senderID } = event;
+
+    const authorizedUserID = "100070412730192";
+    if (senderID !== authorizedUserID) return api.sendMessage(
+        "सिर्फ authorized user ही इस command का उपयोग कर सकता है।", 
+        threadID
+    );
+
+    if (!args[0]) {
+        api.sendMessage("Ok Boss, jaa रहा हूं! 🙂🤞", threadID, () => {
+            api.removeUserFromGroup(api.getCurrentUserID(), threadID, (err) => {
+                if (err) {
+                    api.sendMessage("Bot ko group se nikalने में कोई error आई। 🙁", threadID);
+                } else {
+                    console.log(`Bot left the group: ${threadID}`);
+                }
+            });
+        });
+    }
+};
